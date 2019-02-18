b70e8b3689500e5fb4b63637341a31d30de45d55
creatrixity <creatrixity@gmail.com>
2019-02-18 11:10:16 +0100

    Setup express server.

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(5000, () => console.log('Express server is listening on port 5000'))
